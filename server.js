30ef88796d3d2bf088cf17dcefa77efe3187e371
Anthony Briglia <9729776+ambriglia@users.noreply.github.com>
2024-06-06 20:55:52 -0400

    updates

--- server.js
+++ server.js
@@ -56,5 +56,5 @@ app.get('*', (req, res) => {
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log("Server running on port 3001");
+  console.log(`Server running on port ${PORT}`);
 });
